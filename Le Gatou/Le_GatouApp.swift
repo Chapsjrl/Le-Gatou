ec7aba8e57b917b499ee1f869d996613a0ece999
Alejandro Rivera <gusanomurcielago@hotmail.com>
2023-08-01 21:11:06 -0600

    Initial Commit

---
+++ Le Gatou/Le_GatouApp.swift
@@ -0,0 +1,17 @@
+//
+//  Le_GatouApp.swift
+//  Le Gatou
+//
+//  Created by Alejandro Rivera on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Le_GatouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
